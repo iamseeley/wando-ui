559c8d0c9ab0e01434b71973f3f95d01b2330c76
iamseeley <seeleypersonal@gmail.com>
2023-12-13 12:10:28 -0500

    gonna publish

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
         {/* <Image className="top-4" alt="Cute image of the Wando 'W' " src={'/assets/images/wando.png'} width={100} height={100} /> */}
         <a className="font-bold text-orange-900 text-xl" href="/">wando-ui</a>
         
-			<h1 className='pb-4 text-3xl text-orange-200 font-semibold'>Minimal, component collection.</h1>
+			<h1 className='pb-4 text-3xl text-orange-100 font-semibold'>Minimal, component collection.</h1>
             <div className='flex gap-4'>
                 
                 <Button intent="secondary">Github</Button>
