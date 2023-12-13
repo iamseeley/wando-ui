13a6a49ede6ebd3e590a6c75251eb19a70ebef89
iamseeley <seeleypersonal@gmail.com>
2023-12-13 15:47:15 -0500

    trying to fix cli still

--- cli/src/scripts/init.ts
+++ cli/src/scripts/init.ts
@@ -1,17 +1,11 @@
 import inquirer from 'inquirer';
 import chalk from 'chalk';
 import { execSync } from 'child_process';
-import fsExtra from 'fs-extra';
+import fs from 'fs';
+import fetch, { Response } from 'node-fetch';
 import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export default async function initWandoUI() {
-
-
+export default async function initWandoUI(): Promise<void> {
     console.log("Setting up wando-ui...");
 
     const answers = await inquirer.prompt([
@@ -23,28 +17,46 @@ export default async function initWandoUI() {
         }
     ]);
 
-    const globalsDest = answers.globalsDest;
-    
-    // Install TailwindCSS and other dependencies
+    const globalsDest: string = answers.globalsDest;
+
     console.log(chalk.white("Installing dependencies: tailwindcss, postcss, autoprefixer, class-variance-authority..."));
     execSync('npm install tailwindcss postcss autoprefixer class-variance-authority', { stdio: 'inherit' });
     console.log(chalk.green("Dependencies installed successfully!"));
 
- 
+    const tailwindConfigUrl: string = 'https://raw.githubusercontent.com/iamseeley/wando-ui/tailwind.config.ts';
+    const globalsCssUrl: string = 'https://raw.githubusercontent.com/iamseeley/wando-ui/app/globals.css';
+
+    const currentDirectory: string = process.cwd();
+    const tailwindConfigDest: string = path.join(currentDirectory, 'tailwind.config.ts');
+    const globalStylesDest: string = path.join(currentDirectory, globalsDest, 'globals.css');
 
 
-    // Define source paths (adjust as needed based on your exact directory structure)
-    const tailwindConfigSource = path.join(__dirname, '..', '..', 'tailwind.config.ts');
-    const globalStylesSource = path.join(__dirname, '..', '..', '..', 'wando-ui', 'app', 'globals.css');
+    const downloadFile = async (url: string, dest: string): Promise<void> => {
+        const response: Response = await fetch(url);
+    
+        // Check if the response was successful
+        if (!response.ok) {
+            throw new Error(`Failed to download: ${url} - Response Status: ${response.status}`);
+        }
+    
+        // Use a type assertion to tell TypeScript that response.body is not null
+        const body = response.body as NodeJS.ReadableStream;
+    
+        const fileStream = fs.createWriteStream(dest);
+        await new Promise((resolve, reject) => {
+            body.pipe(fileStream);
+            body.on("error", reject);
+            fileStream.on("finish", resolve);
+        });
+    };
+    
+    
 
-    // Define destination paths
-    const currentDirectory = process.cwd();
-    const tailwindConfigDest = path.join(currentDirectory, 'tailwind.config.ts');
-    const globalStylesDest = path.join(currentDirectory, globalsDest, 'globals.css');
-
-    // Copy the tailwind.config.js and global.css files
-    fsExtra.copySync(tailwindConfigSource, tailwindConfigDest);
-    fsExtra.copySync(globalStylesSource, globalStylesDest);
-
-    console.log("wando-ui initialized successfully!");
-};
+    try {
+        await downloadFile(tailwindConfigUrl, tailwindConfigDest);
+        await downloadFile(globalsCssUrl, globalStylesDest);
+        console.log("wando-ui initialized successfully!");
+    } catch (error) {
+        console.error(chalk.red('Error downloading files:', error));
+    }
+}
